6b74f500fc242f3d2c124949b1dcc41e38dcbba8
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-10-31 03:01:07 +0200

    правки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-const galerryContainer = document.querySelector(".gallery");
-const cardsMarkup = createGalleryItemsMarkup (items);
+const galerryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemsMarkup(items);
 
 galerryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-function createGalleryItemsMarkup (items) {
-    return items.map(({preview, original, description}) => {
+// Створюю розмітку html
+function createGalleryItemsMarkup(items) {
+    return items
+    .map(({ preview, original, description }) => {
         return `
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="large-image.jpg"
-        alt="${description}"
-      />
-    </a>`
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
     })
-    .join('');
-
+      .join("");
 }
+
+console.log(items);
+
+// create gallery items
+
+const instance = basicLightbox.create(`<img src="${e.target.dataset.source}>`,
+{
+  onShow: () => { document.addEventListener("keydown", onEsc) },
+  onClose: () => { document.removeEventListener("keydown", onEsc) }
+});
+
+instance.show()
  
