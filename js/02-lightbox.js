da423c781e042dc62956fe322da4547fa8b03d98
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-11-06 17:10:29 +0200

    updates

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,9 +15,27 @@ function createGalleryItemsMarkup(items) {
     return items
     .map(({ preview, original, description }) => {
         return `<a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}"/>
-      </a>
+        <img 
+          class="gallery__image" 
+          src="${preview}" 
+          alt="${description}"/>
+        </a>
       `;
     })
       .join("");
 }
+
+function onGalleryModalClick(evt) {
+    evt.preventDefault();
+
+    if(evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionPosition: 'bottom',
+    captionsData:'alt', 
+    captionDelay:250,
+});
