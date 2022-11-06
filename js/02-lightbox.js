47db0ca25ccc0f0c8b6a3c8f494f2d7f56b0cdf2
rollingolya <107012291+rollingolya@users.noreply.github.com>
2022-11-06 16:00:35 +0200

    adds gallery to task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryModalClick);
+
+// Створюю розмітку html
+function createGalleryItemsMarkup(items) {
+    return items
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>
+      `;
+    })
+      .join("");
+}
